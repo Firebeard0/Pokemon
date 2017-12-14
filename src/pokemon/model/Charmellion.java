7b681ed0268cc1df3b667d26c244671cef8b912e
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-12-14 13:55:30 -0700

    Wrote the pokemon.model and put in the pokemon types and began to code in pokemon

---
+++ src/pokemon/model/Charmellion.java
@@ -0,0 +1,25 @@
+package pokemon.model;
+
+public class Charmellion extends Pokemon implements FireType
+{
+	public Charmellion(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public String ember(int damage)
+	{
+		String inflicted = "1";
+		return inflicted;
+	}
+	public String flameThrower(int damage)
+	{
+		String inflicted = "1";
+		return inflicted;
+	}
+	public String inferno(int damage)
+	{
+		String inflicted = "1";
+		return inflicted;
+	}
+}
---
+++ src/pokemon/model/WaterType.java
@@ -0,0 +1,8 @@
+package pokemon.model;
+
+public interface WaterType
+{
+	public String drizzle(int damage);
+	public String flood(int damage);
+	public String tsunami(int damage);
+}
